7eb040578495ae54debe1db67408f653cc62aca4
Siddhi Dhairyashil Nikam <siddhinikam1907@gmail.com>
2026-02-07 21:43:32 +0530

    Fix port for deployment

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ Rules:
   }
 });
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
